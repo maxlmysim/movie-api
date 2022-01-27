8936f8cf884bd922a00d26a74a22c84222a5f136
maxlmysim <maxlmysim@gmail.com>
2022-01-27 22:57:03 +0300

    refactor: refactor

--- script/index.js
+++ script/index.js
@@ -1,10 +1,9 @@
 async function getDataMovies(query) {
-    let url = 0;
-    if (!query) {
-        url = 'https://imdb-api.com/API/AdvancedSearch/k_x2rr85bg?groups=now-playing-us'
-    } else {
+    let url = 'https://imdb-api.com/API/AdvancedSearch/k_x2rr85bg?groups=now-playing-us';
+        if (query) {
         url = `https://imdb-api.com/API/AdvancedSearch/k_x2rr85bg?title=${query}&count=10`;  // api: [k_042uslfp]  [k_x2rr85bg]
     }
+
     const res = await fetch(url);
     const data = await res.json();
 
@@ -19,34 +18,6 @@ async function getDataMovies(query) {
     addEvent();
 }
 
-function addEvent() {
-    movieBlocks = document.querySelectorAll('.movie-block');
-    movieBlocks.forEach((item) => {
-        item.addEventListener('mouseenter', function (e) {
-            increaseHeight(e, 'enter');
-        });
-        item.addEventListener('mouseleave', function (e) {
-            increaseHeight(e, 'leave');
-        });
-    });
-}
-
-function increaseHeight(event, mouse) {
-    let movieBlock = event.target;
-    let info = Array.from(movieBlock.childNodes).find((item) => {
-        if (item.classList.contains('info-movie')) return true;
-    });
-    let heightAdd = info.clientHeight;
-
-    if (mouse === 'enter') {
-        movieBlock.style.height = `${heightAdd + 410}px`;
-    }
-
-    if (mouse === 'leave') {
-        movieBlock.style.height = ``;
-    }
-}
-
 function createForm(movie) {
     let block = document.createElement('div');
     block.className = 'movie-block';
@@ -56,65 +27,6 @@ function createForm(movie) {
     block.append(getInfoMovie(movie));
 
     container.append(block);
-}
-
-function watchMovie(movie) {
-    let button = document.createElement('div');
-    button.className = 'button-watch-movie';
-    button.textContent = 'Watch Movie';
-
-    let a = document.createElement('a');
-    a.className = 'link-watch-movie';
-    a.href = `https://rezka.ag/search/?do=search&subaction=search&q=${movie.title}`;
-    a.target = '_blank';
-    a.append(button);
-
-    return a;
-}
-
-function getInfoMovie(movie) {
-    let infoMovie = document.createElement('div');
-
-    infoMovie.className = 'info-movie';
-    infoMovie.append(getTitle(movie));
-    infoMovie.append(getPlotInfo(movie));
-    infoMovie.append(getGenres(movie));
-    infoMovie.append(getStars(movie));
-    infoMovie.append(watchMovie(movie));
-    return infoMovie;
-}
-
-function getStars(movie) {
-    let stars = document.createElement('p');
-    stars.className = 'stars-movie';
-    let starList = movie.starList
-        .slice(1)
-        .map((name) => name.name)
-        .join(', ');
-    stars.innerHTML = `<span class="stars-title">Stars:</span> <span class="stars-info">${starList}</span>`;
-
-    return stars;
-}
-
-function getGenres(movie) {
-    let genres = document.createElement('p');
-    genres.className = 'genres-movie';
-    genres.innerHTML = `<span class="genres-title">Genres:</span> <span class="genres-info">${movie.genres}</span>`;
-    return genres;
-}
-
-function getPlotInfo(movie) {
-    let plotInfo = document.createElement('div');
-    plotInfo.className = 'plot-info';
-    plotInfo.textContent = movie.plot;
-    return plotInfo;
-}
-
-function getTitle(movie) {
-    let h3 = document.createElement('h3');
-    h3.className = 'movie-title';
-    h3.textContent = `${movie.title}`;
-    return h3;
 }
 
 function createBackground(movie) {
@@ -145,6 +57,94 @@ function getRating(movie) {
     }
 
     return rating;
+}
+
+function getInfoMovie(movie) {
+    let infoMovie = document.createElement('div');
+
+    infoMovie.className = 'info-movie';
+    infoMovie.append(getTitle(movie));
+    infoMovie.append(getPlotInfo(movie));
+    infoMovie.append(getGenres(movie));
+    infoMovie.append(getStars(movie));
+    infoMovie.append(watchMovie(movie));
+    return infoMovie;
+}
+
+function getTitle(movie) {
+    let h3 = document.createElement('h3');
+    h3.className = 'movie-title';
+    h3.textContent = `${movie.title}`;
+    return h3;
+}
+
+function getPlotInfo(movie) {
+    let plotInfo = document.createElement('div');
+    plotInfo.className = 'plot-info';
+    plotInfo.textContent = movie.plot;
+    return plotInfo;
+}
+
+function getGenres(movie) {
+    let genres = document.createElement('p');
+    genres.className = 'genres-movie';
+    genres.innerHTML = `<span class="genres-title">Genres:</span> <span class="genres-info">${movie.genres}</span>`;
+    return genres;
+}
+
+
+function getStars(movie) {
+    let stars = document.createElement('p');
+    stars.className = 'stars-movie';
+    let starList = movie.starList
+        .slice(1)
+        .map((name) => name.name)
+        .join(', ');
+    stars.innerHTML = `<span class="stars-title">Stars:</span> <span class="stars-info">${starList}</span>`;
+
+    return stars;
+}
+
+function watchMovie(movie) {
+    let button = document.createElement('div');
+    button.className = 'button-watch-movie';
+    button.textContent = 'Watch Movie';
+
+    let a = document.createElement('a');
+    a.className = 'link-watch-movie';
+    a.href = `https://rezka.ag/search/?do=search&subaction=search&q=${movie.title}`;
+    a.target = '_blank';
+    a.append(button);
+
+    return a;
+}
+
+function addEvent() {
+    movieBlocks = document.querySelectorAll('.movie-block');
+    movieBlocks.forEach((item) => {
+        item.addEventListener('mouseenter', function (e) {
+            increaseHeight(e, 'enter');
+        });
+        item.addEventListener('mouseleave', function (e) {
+            increaseHeight(e, 'leave');
+        });
+    });
+}
+
+function increaseHeight(event, mouse) {
+    let movieBlock = event.target;
+    let info = Array.from(movieBlock.childNodes).find((item) => {
+        if (item.classList.contains('info-movie')) return true;
+    });
+    let heightAdd = info.clientHeight;
+
+    if (mouse === 'enter') {
+        movieBlock.style.height = `${heightAdd + 410}px`;
+    }
+
+    if (mouse === 'leave') {
+        movieBlock.style.height = ``;
+    }
 }
 
 let queryForm = document.querySelector('.header__search');
